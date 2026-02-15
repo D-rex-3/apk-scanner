46e493612373aa5a4b5e5b026a4a331d0170bae9
unknown <sabavathdinesh0@gmail.com>
2026-02-15 16:39:37 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -53,4 +53,5 @@ def analyze():
     return render_template("report.html",findings=findings,risk_score=risk_score)
     
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
